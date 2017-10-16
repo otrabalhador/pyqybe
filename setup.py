4642cd8a1c14b51a1c967b97df1db1ee88b4f39c
Eryk Humberto Oliveira Alves <erykwho@gmail.com>
2017-10-16 10:45:04 -0200

    Change it to version 0.0.2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='pyqybe',
-      version='0.0.1',
+      version='0.0.2',
       author='Eryk Humberto Oliveira Alves',
       author_email='erykwho@gmail.com',
       url='https://github.com/otrabalhador/pyqybe/',
